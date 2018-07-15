459a67f6103b88c912732480c3775387d2b274fa
Youssef Almkari <youssefalmkari@gmail.com>
2018-07-15 11:56:33 -0500

    Toolbar
    Toolbar Menu & Title

--- app/src/main/java/space/personal/youssefalmkari/tictactoe/MainActivity.kt
+++ app/src/main/java/space/personal/youssefalmkari/tictactoe/MainActivity.kt
@@ -1,7 +1,11 @@
 package space.personal.youssefalmkari.tictactoe
 
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,9 +21,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Toolbar
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
 
+        // Floating Action Button
+        val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
             HelloKotlin().displayMessage(view)
         }
     }
+
+    /**
+     * Inflate the Menu; this adds items to the action bar if it is
+     * present.
+     * ------------------------------------------------------------
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    /**
+     * Handle action bar item clicks here. The action bar will
+     * automatically handle clicks on th Home/Up button, so long
+     * as you specify a parent activity in AndroidManifest.xml.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+
 }
