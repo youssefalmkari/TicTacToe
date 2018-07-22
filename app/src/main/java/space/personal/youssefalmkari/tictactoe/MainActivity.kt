5689861d1f9c6bb6e62eb970abbe285fa881ddc7
Youssef Almkari <youssefalmkari@gmail.com>
2018-07-22 16:21:18 -0500

    Cell OnClick Implementation & Title Change Logic

--- app/src/main/java/space/personal/youssefalmkari/tictactoe/MainActivity.kt
+++ app/src/main/java/space/personal/youssefalmkari/tictactoe/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 
 /**
@@ -17,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     /**
      * Holds the TableLayout View
      */
-    var tableLayout: TableLayout? = null
+    private var tableLayout: TableLayout? = null
     /**
      * Displays who's turn it is
      */
@@ -25,11 +28,11 @@ class MainActivity : AppCompatActivity() {
     /**
      * Represents a 3x3 matrix simulating the Board
      */
-    var gameBoard: Array<CharArray> = Array(3) { CharArray(3) }
+    private var gameBoard: Array<CharArray> = Array(3) { CharArray(3) }
     /**
      * Value of current turn
      */
-    var turn = 'X'
+    var turn: Char = 'X'
 
 
     /**
@@ -39,6 +42,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        turnTextView = findViewById(R.id.turnTextView) as TextView
+        tableLayout = findViewById(R.id.table_layout) as TableLayout
+
+        // New Game
+        startNewGame(true)
+
         // Toolbar
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
@@ -72,5 +82,62 @@ class MainActivity : AppCompatActivity() {
         return if (id == R.id.action_settings) {
             true
         } else super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Begins a new game
+     */
+    private fun startNewGame(setClickListener: Boolean) {
+        turn = 'X'
+        turnTextView?.text = String.format(getString(R.string.turn), turn)
+
+        // Row
+        for (i in 0 until gameBoard.size) {
+            // Column
+            for (j in 0 until gameBoard[i].size) {
+                gameBoard[i][j] = ' '
+                val cell = (tableLayout?.getChildAt(i) as TableRow).getChildAt(j) as TextView
+                cell.text = ""
+
+                if (setClickListener) {
+                    cell.setOnClickListener { cellClickListener(i, j) }
+                    // Check if game has a winner
+                }
+            }
+        }
+    }
+
+    /**
+     * What happens when Board Cell is clicked
+     */
+    private fun cellClickListener(row: Int, column: Int) {
+        gameBoard[row][column]
+        ((tableLayout?.getChildAt(row) as TableRow)
+                .getChildAt(column) as TextView).text = turn.toString()
+        turn = if ('X' == turn) 'O' else 'X'
+        turnTextView?.text = String.format(getString(R.string.turn), turn)
+    }
+
+    private fun isBoardFull(gameBoard: Array<CharArray>): Boolean {
+
+        // Row
+        for (i in 0 until gameBoard.size) {
+            // Column
+            for (j in 0 until gameBoard[i].size) {
+                // A cell is empty
+                if (gameBoard[i][j] == ' ') return false
+            }
+        }
+
+        // All Cells in Board are filled
+        return true
+    }
+
+    private fun isWinner(gameBoard: Array<CharArray>, w: Char): Boolean {
+        return false
+    }
+
+    private fun checkGameStatus() {
+        var state: String? = null
     }
 }
